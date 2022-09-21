7c346bd78c8eb3a4b061d9547c9e1dfd1f766121
Matt <matt@spectrebot.net>
2022-09-21 18:34:38 +0100

    add better comments

--- blockpage.js
+++ blockpage.js
@@ -9,17 +9,34 @@ chrome.webRequest.onBeforeRequest.addListener(
     {
       types: ['main_frame'],
       urls: [
+          // Remove all dynadot.com subdomains
           '*://*.dynadot.com/*', 
+          // Remove all dynadot.com domains
           '*://dynadot.com/*', 
+
+          // Remove all porkbun.com subdomains
           '*://*.porkbun.com/*', 
+          // Remove all porkbun.com domains
           '*://porkbun.com/*', 
-          '*://*.godaddy.com/*', 
+
+          // Remove all godaddy.com subdomains
+          '*://*.godaddy.com/*',
+          // Remove all godaddy.com domains 
           '*://godaddy.com/*', 
+
+          // Remove all register.com subdomains
           '*://*.register.com/*', 
+          // Remove all register.com domains
           '*://register.com/*', 
+
+          // Remove all namecheap.com subdomains
           '*://*.namecheap.com/*', 
+          // Remove all namecheap.com domains
           '*://namecheap.com/*', 
+
+          // Remove all iwantmyname.com subdomains
           '*://*.iwantmyname.com/*', 
+          // Remove all iwantmyname.com domains
           '*://*.iwantmyname.com/*'
       ],
     },
