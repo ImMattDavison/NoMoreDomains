8d2b28cd3b9ebadd4f9e50f99c043972c4ef873b
Weredime <100447465+Weredime@users.noreply.github.com>
2022-10-01 13:16:35 +0100

    Move domains into its own file

---
+++ domains.js
@@ -0,0 +1,21 @@
+export default [
+    "// In this file, you can add hostnames to be redirected.",
+    "dynadot.com",
+    "porkbun.com",
+    "godaddy.com",
+    "register.com",
+    "namecheap.com",
+    "iwantmyname.com",
+    "enom.com",
+    "ovhcloud.com",
+    "ionos.com",
+    "ionos.co.uk",
+    "123-reg.com",
+    "123-reg.co.uk",
+    "names.co.uk",
+    "domain.com",
+    "sedo.com",
+    "dan.com",
+    "domains.google",
+    "afternic.com"
+]
