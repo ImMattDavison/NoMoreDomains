6ae31c94ca33fc3e2c89c1426a5f96e36625a645
Matt Davison <matt@mjdws.com>
2022-10-01 19:22:33 +0100

    add keepalive

--- blockpage.js
+++ blockpage.js
@@ -1,3 +1,45 @@
+// Keep service worker alive
+let lifeline;
+
+keepAlive();
+async function keepAlive() {
+  if (lifeline) return;
+  for (const tab of await chrome.tabs.query({ url: '*://*/*' })) {
+    try {
+      await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => {
+          chrome.runtime.connect({ name: 'keepAlive' });
+          console.log('keepAlive');
+        },
+      });
+      chrome.tabs.onUpdated.removeListener(retryOnTabUpdate);
+      return;
+    } catch (e) {}
+  }
+  chrome.tabs.onUpdated.addListener(retryOnTabUpdate);
+}
+
+function keepAliveForced() {
+  lifeline?.disconnect();
+  lifeline = null;
+  keepAlive();
+}
+
+async function retryOnTabUpdate(tabId, info, tab) {
+  if (info.url && /^(file|https?):/.test(info.url)) {
+    keepAlive();
+  }
+}
+
+chrome.runtime.onConnect.addListener(port => {
+  if (port.name === 'keepAlive') {
+    lifeline = port;
+    setTimeout(keepAliveForced, 295e3);
+    port.onDisconnect.addListener(keepAliveForced);
+  }
+});
+
 // Handle web requests and block domain registrars
 fetch("/domains.json")
   .then((res) => res.json())
